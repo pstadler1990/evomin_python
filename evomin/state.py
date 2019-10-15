25d92fe40b3c43a7f862ae2ae5f0a41b6af0c95d
Patrick Stadler <patrick.stadler@evopro-ag.de>
2019-10-15 14:35:35 +0200

    added state details

--- evomin/state.py
+++ evomin/state.py
@@ -1,6 +1,8 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*
 from abc import ABC, abstractmethod
+from typing import Any, Optional
+from evomin.frame import EvominFrameMessageType
 
 
 class State(ABC):
@@ -10,9 +12,14 @@ class State(ABC):
     proceed: translate to the next state
     fail: fail the current state
     """
-    @abstractmethod
-    def run(self, byte: int) -> None:
-        pass
+    def __init__(self, expected: Optional[Any] = None) -> None:
+        self.expected_byte = expected
+
+    def run(self, byte: int) -> 'State':
+        if (not self.expected_byte) or (self.expected_byte and byte == self.expected_byte):
+            return self.proceed()
+        else:
+            return self.fail()
 
     @abstractmethod
     def proceed(self) -> 'State':
@@ -21,135 +28,107 @@ class State(ABC):
     @abstractmethod
     def fail(self) -> 'State':
         pass
+
+    @property
+    def expect(self):
+        return self.expected_byte
+
+    @expect.setter
+    def expect(self, expected):
+        self.expected_byte = expected
 
 
 class StateIdle(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
 
 
 class StateSof(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
 
 
 class StateSof2(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
 
 
 class StateCmd(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
 
 
 class StateLen(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
 
 
 class StatePayld(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
 
 
 class StateCRC(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
 
 
 class StateCRCFail(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
 
 
 class StateEof(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
 
 
 class StateReply(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
 
 
 class StateReplyCreateFrame(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
 
 
 class StateError(State):
-    def run(self, byte: int) -> None:
-        pass
-
     def proceed(self) -> 'State':
-        pass
+        print("State idle PROCEED")
 
     def fail(self) -> 'State':
-        pass
+        print("State idle FAIL")
