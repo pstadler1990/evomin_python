91e14fde95805becb27b437c0bfbb79cf3d7dc76
Patrick Stadler <patrick.stadler@evopro-ag.de>
2019-10-15 16:41:12 +0200

    added logic for all states (still wip)

--- evomin/evomin.py
+++ evomin/evomin.py
@@ -101,22 +101,22 @@ class StateMachine:
             return self.state_machine.state_len
 
         def fail(self) -> 'State':
-            print("State idle FAIL")
+            print("State cmd FAIL")
             return self.state_machine.state_error
 
     class StateLen(State):
         def proceed(self, byte: int) -> 'State':
-            print("State idle PROCEED")
-            self.state_machine.interface.current_frame.payload_len = byte
+            print("State len PROCEED")
+            self.state_machine.interface.current_frame.payload_length = byte
             return self.state_machine.state_payld if byte > 0 else self.state_machine.state_crc
 
         def fail(self) -> 'State':
-            print("State idle FAIL")
+            print("State len FAIL")
             return self.state_machine.state_error
 
     class StatePayld(State):
         def proceed(self, byte: int) -> 'State':
-            print("State idle PROCEED")
+            print("State payld PROCEED")
             # For the reception of a frame body if two 0xAA bytes in a row are received
             # then the next received byte is discarded
             if self.state_machine.interface.current_frame.last_byte_was_stfbyt:
@@ -132,59 +132,110 @@ class StateMachine:
             if self.state_machine.interface.current_frame.payload_buffer.size < self.state_machine.interface.current_frame.payload_length:
                 try:
                     self.state_machine.interface.current_frame.payload_buffer.push(byte)
+                    return self.state_machine.state_payld
                 except Full:
-                    self.fail()
+                    return self.fail()
+            else:
+                if self.state_machine.interface.com_interface.describe().is_master_slave:
+                    # On a master-slave communication interface, call the frame_received handler early, to allow
+                    # the slave to prepare a reply
+                    # TODO: Call frame_received()
+                    pass
+                return self.state_machine.state_crc
 
         def fail(self) -> 'State':
-            print("State idle FAIL")
+            print("State payld FAIL")
             return self.state_machine.state_error
 
     class StateCRC(State):
         def proceed(self, byte: int) -> 'State':
-            print("State idle PROCEED")
+            print("State crc PROCEED")
+            self.state_machine.interface.current_frame.crc8 = byte
+            frame_calculated_crc8: int = 86  # TODO: Replace 1 with calculate_crc8(..)
+            if byte == frame_calculated_crc8:
+                self.state_machine.interface.current_frame.is_valid = True
+                # TODO: If master-slave mode:
+                    # TODO: Send ACK byte to acknowledge reception of message (pre-fill TX buffer to be ready at the EOF byte)
+                return self.state_machine.state_eof
+            else:
+                # TODO: If master-slave mode:
+                    # TODO: Send NACK byte to cancel any further sender communication
+                return self.fail()
 
         def fail(self) -> 'State':
-            print("State idle FAIL")
+            print("State crc FAIL")
+            return self.state_machine.state_crc_fail
 
     class StateCRCFail(State):
         def proceed(self, byte: int) -> 'State':
-            print("State idle PROCEED")
+            print("State crcfail PROCEED")
+            return self.fail()
 
         def fail(self) -> 'State':
-            print("State idle FAIL")
+            print("State crcfail FAIL")
+            # Call the error state directly, as there's no further data reception after this state
+            self.state_machine.state_error.run(0)
+            # We can safely return to the idle state here
+            return self.state_machine.state_idle
 
     class StateEof(State):
         def proceed(self, byte: int) -> 'State':
-            print("State idle PROCEED")
+            print("State eof PROCEED")
+            if self.state_machine.interface.current_frame.is_valid:
+                if self.state_machine.interface.com_interface.describe().is_master_slave:
+                    # TODO: Send number of reply bytes
+                    return self.state_machine.state_reply
+                else:
+                    # TODO: Send ACK
+                    # TODO: Call frame_received()
+                    return self.state_machine.state_idle
+            else:
+                return self.fail()
 
         def fail(self) -> 'State':
-            print("State idle FAIL")
+            print("State eof FAIL")
+            return self.state_machine.state_error
 
     class StateReply(State):
         def proceed(self, byte: int) -> 'State':
-            print("State idle PROCEED")
+            print("State reply PROCEED")
+            # TODO: Send answer bytes
+            return self.state_machine.state_idle    # TODO
 
         def fail(self) -> 'State':
-            print("State idle FAIL")
+            print("State reply FAIL")
+            return self.state_machine.state_error
 
     class StateReplyCreateFrame(State):
         def proceed(self, byte: int) -> 'State':
-            print("State idle PROCEED")
+            print("State replycreateframe PROCEED")
+            return self.state_machine.state_idle  # TODO
 
         def fail(self) -> 'State':
-            print("State idle FAIL")
+            print("State replycreateframe FAIL")
+            return self.state_machine.state_error
 
     class StateError(State):
         def proceed(self, byte: int) -> 'State':
-            print("State idle PROCEED")
+            print("State error PROCEED")
+            # TODO: Send NACK
+            # TODO: Add logger entry
+            return self.state_machine.state_idle
 
         def fail(self) -> 'State':
-            print("State idle FAIL")
+            print("State error FAIL")
+            return self.state_machine.state_error
 
 
 class Evomin:
-
+    """
+    Evomin is the main entry point to the evomin communication interface
+    """
     def __init__(self, com_interface: EvominComInterface) -> None:
+        """
+        Initialize the evomin communication interface
+        :param com_interface: An instance of a communication interface implementation (refer to EvominComInterface)
+        """
         self.com_interface: EvominComInterface = com_interface
         self.frame_send_queue: Queue = Queue(maxsize=config['interface']['max_queued_frames'])
         self.current_frame: EvominFrame = type(None)
@@ -192,8 +243,15 @@ class Evomin:
         self.byte_getter = self.com_interface.receive_byte()
 
     def rx_handler(self) -> None:
-        incoming_byte: int = next(self.byte_getter)
-        if incoming_byte >= 0:
-            # Byte received
-            print("Received byte: ", incoming_byte)
-            self.state.run(incoming_byte)
+        """
+        This handler needs to be called periodically (or in a thread / interrupt), i.e. polling, to receive / transmit
+        and process bytes and the higher level EvominFrames.
+        """
+        try:
+            incoming_byte: int = next(self.byte_getter)
+            if incoming_byte >= 0:
+                # Byte received
+                print("Received byte: ", incoming_byte)
+                self.state.run(incoming_byte)
+        except StopIteration:
+            pass
