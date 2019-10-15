11f5b1b66f3e5e9f75f34826469e681d6a1d2e96
Patrick Stadler <patrick.stadler@evopro-ag.de>
2019-10-15 12:05:40 +0200

    implemented initialization

--- evomin/buffer.py
+++ evomin/buffer.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
+from queue import Queue
+from evomin.config import config
+
+
+class EvominBuffer:
+
+    def __init__(self, initial_bytes: bytes = bytes([])):
+        self.buffer: Queue = Queue(maxsize=config['frame']['buffer_size'])
+        map(self.buffer.put, initial_bytes)
