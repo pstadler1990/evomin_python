58753091a6181ec9e8f22fc7458415b42895563b
Patrick Stadler <patrick.stadler@evopro-ag.de>
2019-10-16 15:41:20 +0200

    now using absolute path to prevent FileNotFound errors

--- evomin/config.py
+++ evomin/config.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 import yaml
+import os
 
-config = yaml.safe_load(open('./evomin/config.yml'))
+config = yaml.safe_load(open(os.path.join(os.path.dirname(__file__), 'config.yml')))
