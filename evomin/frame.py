22844259fa75c33eb46d6f91f0388a590c9dcd6e
Patrick Stadler <patrick.stadler@evopro-ag.de>
2019-10-16 11:02:48 +0200

    modified payload getters / setters

--- evomin/frame.py
+++ evomin/frame.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
+from enum import Enum
 from typing import Optional, Generator
 from evomin.buffer import EvominBuffer
 from datetime import datetime
@@ -25,7 +26,7 @@ class EvominFrameMessageType:
     DUMMY = 0xF0
 
 
-class EvominFrameCommandType:
+class EvominFrameCommandType(Enum):
     """
     EvominFrameCommandType defines all protocol internal command types.
     These can be extended, but sometimes it's better to use a predefined command and add a payload, than extending
@@ -49,7 +50,7 @@ class EvominFrame:
         """
         self.is_sent: bool = False
         self.is_valid: bool = False
-        self.command: int = command if command in [EvominFrameCommandType] else EvominFrameCommandType.RESERVED
+        self.command: int = command if command in [c.value for c in EvominFrameCommandType] else EvominFrameCommandType.RESERVED
         self.payload_buffer: EvominBuffer = EvominBuffer(payload)
         self.expected_payload_len: int = len(payload) if payload else 0
         self.crc8: int = 0
@@ -57,17 +58,53 @@ class EvominFrame:
         self.retries_left: int = config['frame']['retry_count']
         self.last_byte_was_stfbyt: bool = False
         self.last_byte: int = -1
+
+        self._calculate_frame()
         # answerBuffer ?
         # replyBuffer ?
 
-    @property
-    def payload(self) -> Generator[int, None, None]:
+    def _calculate_frame(self):
+        payload_tmp: list = []
+
+        crc_tmp: list = [
+            self.command,
+            self.payload_length,
+        ]
+
+        found_pl_header: bool = False
+        last_byte: int = -1
+        for b in self.payload_buffer.buffer.queue:
+            if found_pl_header:
+                payload_tmp.append(EvominFrameMessageType.STFBYT)
+                last_byte = EvominFrameMessageType.STFBYT
+                found_pl_header = False
+            # For the transmission of a frame body if two 0xAA bytes in a row are transmitted a stuff byte
+            # with value 0x55 is inserted into the transmitted byte stream
+            if b == EvominFrameMessageType.SOF and last_byte == EvominFrameMessageType.SOF:
+                found_pl_header = True
+
+            crc_tmp.append(b)
+            payload_tmp.append(b)
+
+        self.payload_buffer.reset()
+        map(self.payload_buffer.push, payload_tmp)
+        self.crc8 = self.calculate_crc8(bytes(crc_tmp))
+        print('> Calculated crc8: ', self.crc8)
+
+    def get_payload(self) -> Generator[int, None, None]:
         for b in self.payload_buffer.buffer.queue:
             yield b
 
-    @payload.setter
-    def payload(self, payload: bytes):
-        map(self.payload_buffer.buffer.put, payload)
+    def set_payload(self, payload: bytes) -> None:
+        self.payload_buffer.reset()
+        self.add_payload(payload)
+        self._calculate_frame()
+
+    def add_payload(self, payload_byte: int) -> None:
+        self.payload_buffer.push(payload_byte)
+
+    def finalize(self) -> None:
+        self._calculate_frame()
 
     @property
     def payload_length(self) -> int:
@@ -76,3 +113,15 @@ class EvominFrame:
     @payload_length.setter
     def payload_length(self, payload_len: int) -> None:
         self.expected_payload_len = payload_len
+
+    @staticmethod
+    def calculate_crc8(stripped_payload: bytes) -> int:
+        crc_initial: int = 0x00
+        for b in stripped_payload:
+            crc_initial ^= b
+            for f in range(8):
+                if crc_initial & 1:
+                    crc_initial = (crc_initial >> 1) ^ 0x8C
+                else:
+                    crc_initial >>= 1
+        return crc_initial
