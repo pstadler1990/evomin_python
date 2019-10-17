7384f4428514d7c65974310d3d29347fdd10cf87
Patrick Stadler <patrick.stadler@evopro-ag.de>
2019-10-17 10:29:18 +0200

    changed polling

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
+from time import sleep
+
 from evomin.com_fake import EvominFakeSPIInterface
 from evomin.evomin import Evomin
-from evomin.frame import EvominFrame
+from evomin.frame import EvominFrame, EvominFrameCommandType
 
 
 class EvominImpl(Evomin):
@@ -12,6 +14,11 @@ class EvominImpl(Evomin):
     def frame_received(self, frame: EvominFrame) -> None:
         print('** Frame received **')
         self.log_debug('[Evomin] EvominFrame received, Command: {}, Payload length: {}'.format(frame.command, frame.payload_length))
+
+        print('Payload: ')
+        for b in frame.get_payload():
+            print(b)
+
         # self.reply(bytes([0xA0, 0xA1, 0xA2, 0xA3, 0xB0, 0xB1, 0xB2, 0xB3]))
         self.reply(bytes([0x01, 0x02]))
 
@@ -20,6 +27,10 @@ if __name__ == '__main__':
     # Initialize evomin communication interface with SPI transport
     evomin = EvominImpl(com_interface=EvominFakeSPIInterface())
 
-    # Mock polling interface
+    # Polling interface
     while True:
-        evomin.rx_handler()
+        evomin.poll()
+
+        evomin.send(EvominFrameCommandType.SEND_IDN, bytes([0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xBB, 0xFF]))
+
+        sleep(1)
