f1c4e7b7ce029f34effb12c9052cb582e9a13efd
Patrick Stadler <patrick.stadler@evopro-ag.de>
2019-10-18 14:50:22 +0200

    implemented mocked spi device

--- evomin/com_fake.py
+++ evomin/com_fake.py
@@ -7,14 +7,16 @@ from evomin.communication import EvominComInterface, ComDescription
 class EvominFakeSPIInterface(EvominComInterface):
     """
     Fake implementation of the EvominComInterface to mock SPI receive and transmission.
+    This simulates master-slave SPI communication where only the master can send bytes on it's own, while the
+    connected slave can only reply to a received byte.
+    This differs from a non master-slave communication, like UART, as in such every connected participant is able to
+    send bytes independently.
     """
     def __init__(self):
-        # self.test_data = bytes([0xAA, 0xAA, 0xAA, 0xCD, 0x4, 0xDE, 0xAD, 0xBE, 0xEF, 0x4E, 0x55, 0x55, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0])
-        # self.test_data = bytes([0xAA, 0xAA, 0xAA, 0xCD, 0x4, 0xDE, 0xAD, 0xBE, 0xEF, 0x4E, 0x55, 0x55])
-        # self.test_data = bytes([0xAA, 0xAA, 0xAA, 0xCD, 0x0, 0x4E, 0x55, 0x55])
-        # self.test_data = bytes([0xAA, 0xAA, 0xAA, 0xCD, 0x0, 0x3D, 0x55, 0x55, 0xF0])
-        # self.test_data = bytes([0xAA, 0xAA, 0xAA, 0xCD, 0x4, 0xAA, 0xAA, 0x55, 0xBB, 0xBB, 0xD7, 0x55, 0x55, 0xF0])
-        self.test_data: bytes = bytes([170, 170, 170, 205, 8, 170, 170, 85, 170, 170, 85, 170, 170, 85, 187, 255, 159, 85, 85])
+        # Mock test data using a generator                                                                ACK  #AnsBytes    reply
+        # The first actual response byte is read on the crc                                                |   |  ___________|__________
+        self.test_data: bytes = bytes([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0xF1, 0xFF, 4, 0xDE, 0xAD, 0xBE, 0xEF])
+        self.receive_iterator = self.receive_byte()
 
     def describe(self):
         return ComDescription(is_master_slave=True)
@@ -24,8 +26,12 @@ class EvominFakeSPIInterface(EvominComInterface):
 
         # Possible implementation of SPI receive / transmit at the same time
         # byte_in = spi_rxtx(byte_out)
-        # return byte_in
-        return 0
+        try:
+            byte_in: int = next(self.receive_iterator)
+            print('Received response byte in: ', byte_in)
+            return byte_in
+        except StopIteration:
+            return None
 
     def receive_byte(self) -> Generator[int, None, None]:
         for b in self.test_data:
